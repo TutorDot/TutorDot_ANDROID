bc6dfe0a1847c10c2894333d082b6d4814ccb6e5
Jungmin <ljm9748@naver.com>
2020-07-07 00:38:31 +0900

    Update activity_login Update bottomnavigation(size)

--- app/src/main/java/com/tutor/tutordot/LoginActivity.kt
+++ app/src/main/java/com/tutor/tutordot/LoginActivity.kt
@@ -1,11 +1,18 @@
 package com.tutor.tutordot
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-    }
+
+ }
+
 }
+
