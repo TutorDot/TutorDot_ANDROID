7761f1772b6426beb5920023f917835e2ed6fe87
teagh82 <teagh8237@gmail.com>
2020-07-07 16:31:15 +0900

    Add, Update classLogView (buttonEvent, 중첩RecyclerView)

--- app/src/main/java/com/tutor/tutordot/ClassLogFragment.kt
+++ app/src/main/java/com/tutor/tutordot/ClassLogFragment.kt
@@ -1,18 +1,22 @@
 package com.tutor.tutordot
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.tutor.tutordot.R
 import kotlinx.android.synthetic.main.fragment_class_log.*
+import kotlinx.android.synthetic.main.item_logdate.*
 
 
 class ClassLogFragment : Fragment() {
 
-    lateinit var logAdapter: LogAdapter
-    val datas : MutableList<LogData> = mutableListOf<LogData>()
+    lateinit var logdateAdapter: LogdateAdapter
+    val datedatas : MutableList<LogdateData> = mutableListOf<LogdateData>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,42 +27,30 @@ class ClassLogFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        logAdapter = LogAdapter(view.context)
-        rv_log.adapter = logAdapter //리사이클러뷰의 어댑터를 지정해줌
-        loadDatas() //데이터를 어댑터에 전달
+        logdateAdapter = LogdateAdapter(view.context)
+        rv_datelog.adapter = logdateAdapter //리사이클러뷰의 어댑터를 지정해줌
+        loaddateDatas() //데이터를 어댑터에 전달
     }
 
-    private fun loadDatas(){
-        datas.apply {
+    private fun loaddateDatas(){
+        datedatas.apply {
             add(
-                LogData(times = 1,
-                    studytime = 1,
-                    alltime = 15,
-                    progress = "수학",
-                    homework = "수학의 정석 풀기",
-                    complete = 1
-                )
-            )
+                LogdateData(
+                    month = 5,
+                    day = 5
+                ))
             add(
-                LogData(times = 2,
-                    studytime = 2,
-                    alltime = 15,
-                    progress = "수학2",
-                    homework = "수학의 정석 풀기2",
-                    complete = 2
-                )
-            )
+                LogdateData(
+                    month = 6,
+                    day = 6
+                ))
             add(
-                LogData(times = 3,
-                    studytime = 3,
-                    alltime = 15,
-                    progress = "수학3",
-                    homework = "수학의 정석 풀기3",
-                    complete = 3
-                )
-            )
+                LogdateData(
+                    month = 7,
+                    day = 7
+                ))
         }
-        logAdapter.datas = datas
-        logAdapter.notifyDataSetChanged()
+        logdateAdapter.datas = datedatas
+        logdateAdapter.notifyDataSetChanged()
     }
 }
