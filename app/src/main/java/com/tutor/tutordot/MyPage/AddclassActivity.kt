3d0acc6f08b6a9c456d3ba5ea19d23e9d5267aba
Jungmin <ljm9748@naver.com>
2020-07-10 07:35:50 +0900

    Add AddclassActivity.kt

--- app/src/main/java/com/tutor/tutordot/MyPage/AddclassActivity.kt
+++ app/src/main/java/com/tutor/tutordot/MyPage/AddclassActivity.kt
@@ -2,6 +2,7 @@ package com.tutor.tutordot.MyPage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tutor.tutordot.R
 
 class AddclassActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
