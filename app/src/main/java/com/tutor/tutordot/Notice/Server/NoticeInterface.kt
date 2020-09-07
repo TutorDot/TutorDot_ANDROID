56c3ba67ca7be8def432b477b6068d208d57dda5
teagh82 <teagh8237@gmail.com>
2020-09-08 01:39:58 +0900

    notice toggle server

--- app/src/main/java/com/tutor/tutordot/Notice/Server/NoticeInterface.kt
+++ app/src/main/java/com/tutor/tutordot/Notice/Server/NoticeInterface.kt
@@ -1,10 +1,12 @@
 package com.tutor.tutordot.Notice.Server
 
+import com.tutor.tutordot.ClassLog.Server.DiaryResponse
 import com.tutor.tutordot.ClassLog.Server.LectureResponse
 import com.tutor.tutordot.ClassLog.Server.LogResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface NoticeInterface {
     @GET("/notice")
@@ -12,4 +14,7 @@ interface NoticeInterface {
 
     @GET("/lecture/name")
     fun lectureRequest(@Header("jwt") header: String) : Call<LectureResponse>
+
+    @GET("/notice/{lid}")
+    fun lecnotiRequest(@Header("jwt") header: String, @Path("lid") path: String) : Call<LecnotiResponse>
 }
